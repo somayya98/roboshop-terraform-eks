12bcb4a5d4f9e53d9cf81152f2b64a1ccc35e6f1
somayya <somayya.adusumalli98@gmail.com>
2025-12-20 12:54:24 +0530

    k8s

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 21.0" # this is module version
 
   name               =   local.common_name_suffix
-  kubernetes_version = "1.33"
+  kubernetes_version = "1.32"
   # kubernetes_version = var.eks_version
 
   addons = {
