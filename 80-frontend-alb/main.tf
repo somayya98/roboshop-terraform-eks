7227fe0be35553d49a5a4753498d1908138d73dc
somayya <somayya.adusumalli98@gmail.com>
2026-01-07 19:41:13 +0530

    jenkins

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -45,3 +45,39 @@ resource "aws_route53_record" "ingress_alb" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_lb_target_group" "frontend" {
+  name     = "${local.common_name_suffix}-frontend"
+  port     = 8080
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60 # waiting period before deleting the instance
+
+  health_check {
+    healthy_threshold = 2
+    interval = 10
+    matcher = "200-299"
+    path = "/"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "frontend" {
+  listener_arn = aws_lb_listener.ingress_alb.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.environment}.${var.domain_name}"] # dev.daws86s.fun
+    }
+  }
+}
